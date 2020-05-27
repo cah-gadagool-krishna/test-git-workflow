62c20573ba6df168e3c8bda0b83a19088bafa9fe
Krishna Gadagool <gadagool.krishna@gmail.com>
2020-05-27 15:23:25 +0530

    Release 2.0.0

--- src/com/cah/Main.java
+++ src/com/cah/Main.java
@@ -4,7 +4,7 @@ package com.cah;
 Main method is here
  */
 public class Main {
-    String appversion = "1.0.0";
+    String appversion = "2.0.0";
 
     public static void main(String[] args) {
         System.out.println("Welcome to Git Workflow Training");
