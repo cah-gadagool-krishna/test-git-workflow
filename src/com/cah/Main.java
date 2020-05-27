dbff76936f7e75ed992f1176994f9d38137289a7
Krishna Gadagool <gadagool.krishna@gmail.com>
2020-05-27 15:26:26 +0530

    Release 1.0.1 Hotfix

--- src/com/cah/Main.java
+++ src/com/cah/Main.java
@@ -4,10 +4,12 @@ package com.cah;
 Main method is here
  */
 public class Main {
-    String appversion = "1.0.0";
+    String appversion = "1.0.1";
+
 
     public static void main(String[] args) {
         System.out.println("Welcome to Git Workflow Training");
+        System.out.println("This is hotfix");
 
     }
 }
